97a40964ef4ae71320a58528b49fa1b0952c1f4e
Michael Vogt <michael.vogt@ubuntu.com>
2011-03-15 15:34:24 +0100

    add copyright headers

--- apt_clone.py
+++ apt_clone.py
@@ -1,5 +1,20 @@
-#!/usr/bin/python
-
+# Copyright (C) 2011 Canonical
+#
+# Authors:
+#  Michael Vogt
+#
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation; version 3.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
 
 import apt
 import apt_pkg
